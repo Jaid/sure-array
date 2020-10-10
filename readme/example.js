8c23a3d984227b1827eb9cbe775e69389c5ec483
Jaid <jaid.jsx@gmail.com>
2020-10-10 05:51:45 +0200

    Updated readme source

--- readme/example.js
+++ readme/example.js
@@ -3,3 +3,6 @@ import sureArray from "sure-array"
 const result = sureArray(123)
 const result2 = sureArray([123])
 const result3 = sureArray()
+const result4 = sureArray(undefined)
+const result5 = sureArray(null)
+const result5 = sureArray([[123]])
